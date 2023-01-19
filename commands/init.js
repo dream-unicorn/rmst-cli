9e901c3d1c3fdff475add60cb8340e48b680a7b1
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-01-19 19:17:43 +0800

    t

--- commands/init.js
+++ commands/init.js
@@ -10,6 +10,8 @@ module.exports = projectName => {
   const rmstCliPackageJson = fse.readJsonSync(path.resolve(__dirname, '../package.json'))
   const templatePackageJson = fse.readJsonSync(path.resolve(templatePath, 'package.json'))
 
+  console.log('rmstCliPackageJson.version: ', rmstCliPackageJson.version)
+
   templatePackageJson.devDependencies['rmst-cli'] = rmstCliPackageJson.version
   templatePackageJson.name = projectName
   fse.writeJSONSync(destPath + '/package.json', templatePackageJson, { spaces: 2 })
